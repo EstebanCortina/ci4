1d4e1b6c084f506393d40e8b2c36c40ad62699fc
Esteban Cortina <estebannsebastian@gmail.com>
2023-06-09 11:01:29 -0600

    Add(Route /form): Creacion de una nueva ruta y vista form

--- firstApp/app/Config/Routes.php
+++ firstApp/app/Config/Routes.php
@@ -30,6 +30,7 @@ $routes->set404Override();
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
+$routes->get('/form', 'Home::form');
 
 /*
  * --------------------------------------------------------------------
@@ -45,5 +46,5 @@ $routes->get('/', 'Home::index');
  * needing to reload it.
  */
 if (is_file(APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php')) {
-    require APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php';
+  require APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php';
 }
---
+++ firstApp/app/Views/form.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <section>
+    <div>
+      <form>
+        <label for="userName">Nombre:</label>
+        <input type="text" name="userName">
+      </form>
+    </div>
+  </section>
+</body>
+
+</html>
