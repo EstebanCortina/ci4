dcf569ad8147cc19f72c7fc6a1f4dc9d2e7d65cb
Esteban Cortina <estebannsebastian@gmail.com>
2023-06-09 11:56:06 -0600

    Update(/form): Ahora form tiene su propio controlador, ruta y vista en carpeta

--- firstApp/app/Views/form/index.php
+++ firstApp/app/Views/form/index.php
@@ -13,6 +13,7 @@
       <form>
         <label for="userName">Nombre:</label>
         <input type="text" name="userName">
+        <button type="button">Enviar</button>
       </form>
     </div>
   </section>
